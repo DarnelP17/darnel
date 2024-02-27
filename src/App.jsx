3a6f1e2e4120a80548e834f65bee1d6354c839fb
DarnelP17 <egidedarnel@gmail.com>
2024-02-27 16:28:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Component } from 'Components/Acceil.jsx'
+
+
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path ="\" >= (/Acceil)</Route>
+        <Route path ="element\" >= (/Contact)</Route>
+      </Routes>  
+    </>
+  )
+}
+
+export default App
